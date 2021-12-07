6556bcdb901f0d1a55b8625f02e31c712e6de133
rafal.slabon <rafal.slabon@ocado.com>
2021-12-07 13:39:37 +0100

    day 7 - done

---
+++ test/aoc2021/day07_test.clj
@@ -0,0 +1,39 @@
+(ns aoc2021.day07_test
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def example-positions [16 1 2 0 4 2 7 1 2 14])
+(def puzzle-input (slurp (io/resource "day07.txt")))
+(def puzzle-positions (map read-string (str/split puzzle-input #",")))
+
+(defn find-min [cost-fn positions all-possible-positions current-min]
+  (if (empty? all-possible-positions)
+    current-min
+    (let [p (first all-possible-positions)
+          cost (reduce + (map #(cost-fn (Math/abs (- % p))) positions))]
+      (if (< cost current-min)
+        (find-min cost-fn positions (rest all-possible-positions) cost)
+        current-min
+        ))))
+
+(defn solve-1 [positions]
+  (find-min identity positions (range (apply min positions) (apply max positions)) Long/MAX_VALUE))
+
+(deftest solve-1-test
+  (testing "solve-1"
+    (is (= 37 (solve-1 example-positions)))
+    (is (= 340056 (solve-1 puzzle-positions)))
+    ))
+
+(defn sum [n] (* (/ (+ 1 n) 2) n))
+
+(defn solve-2 [positions]
+  (find-min sum positions (range (apply min positions) (apply max positions)) Long/MAX_VALUE)
+  )
+
+(deftest solve-2-test
+  (testing "solve-2"
+    (is (= 168 (solve-2 example-positions)))
+    (is (= 96592275 (solve-2 puzzle-positions)))
+    ))
